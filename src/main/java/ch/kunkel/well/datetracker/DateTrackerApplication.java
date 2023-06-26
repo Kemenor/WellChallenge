a81a34b1788d5c1fcca79e2f1028c6922b6f41ae
Thomas <Thomas.Kunkel.tk@gmail.com>
2023-06-26 19:06:03 +0200

    Initial add of Spring Initializer created Project.

---
+++ src/main/java/ch/kunkel/well/datetracker/DateTrackerApplication.java
@@ -0,0 +1,13 @@
+package ch.kunkel.well.datetracker;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DateTrackerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DateTrackerApplication.class, args);
+	}
+
+}
---
+++ src/test/java/ch/kunkel/well/datetracker/DateTrackerApplicationTests.java
@@ -0,0 +1,13 @@
+package ch.kunkel.well.datetracker;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DateTrackerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
