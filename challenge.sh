08cb688c0d1f74172f788c715081cfca806499e4
Thomas <Thomas.Kunkel.tk@gmail.com>
2023-06-28 19:10:41 +0200

    make script wait for container to be running

--- challenge.sh
+++ challenge.sh
@@ -2,11 +2,17 @@
 #gradle jibDockerBuild
 # We could seperate test into unit tests and integrations tests but I don't want to spend too much time with setup
 ./gradlew build test jibDockerBuild
-docker run -p 8090:8090 kunkel/datetracker &
+containerId=$(docker run -p 8090:8090 -d kunkel/datetracker)
+sleep 5
+# Wait for docker container to be running
+# if we add health checks we could wait for healthy
+until [ "`docker inspect -f {{.State.Running}} $containerId`"=="true" ]; do
+    sleep 2
+done;
 
-sleep 10
 # add goss not done yet
 #goss --vars-inline "expected: 2023-06-26" validate
+echo "would be running tests"
 
 #shutdown and remove the container at the end
 docker rm $(docker stop $(docker ps -a -q --filter ancestor=kunkel/datetracker --format="{{.ID}}"))
